848281ed01a063ac0768f8569e3ab90f63e6da16
solim1234 <c58136583@gmail.com>
2025-06-13 17:43:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Futbol',
+    'Quantos gols tem o Neymar?',
+    'O jogador Neymar tem 442 gols na carreira'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos países existem no mundo?',
+    'Existem 195 países no mundo'
+)
+
+criaCartao(
+    'Games',
+    'Qual o jogo mais famoso do mundo?',
+    'O jogo mais famoso do mundo é o Minecraft'
+)
+
+criaCartao(
+    'Música',
+    'Qual a música mais famoso do mundo?',
+    'A música mais famosa do mundo é Despacito,e  dLuis Fonsi com participação de Daddy Yankee'
+)
+
